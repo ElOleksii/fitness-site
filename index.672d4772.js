4b2eb2a8081719b83115622207a80546dc647572
Telenchak Oleksiy <letel315@gmail.com>
2024-09-09 16:07:48 +0300

    Fix icons

---
+++ index.672d4772.js
@@ -0,0 +1,12 @@
+window.addEventListener("resize", (e)=>{
+    const burgerContent = document.querySelector("#burger-content");
+    if (window.innerWidth >= 1024) burgerContent.open = true;
+    else burgerContent.open = false;
+});
+window.addEventListener("DOMContentLoaded", (e)=>{
+    const burgerContent = document.querySelector("#burger-content");
+    if (window.innerWidth >= 1024) burgerContent.open = true;
+    else burgerContent.open = false;
+});
+
+//# sourceMappingURL=index.672d4772.js.map
