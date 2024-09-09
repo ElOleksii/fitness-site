1b51fde8d6ae758412f7a1fd493d5633570867ef
Telenchak Oleksiy <letel315@gmail.com>
2024-09-09 16:00:05 +0300

    Fix bug

--- index.2de562a6.js
+++ index.2de562a6.js
@@ -1,2 +1,9 @@
-window.addEventListener("resize",e=>{let n=document.querySelector("#burger-content");window.innerWidth>=1024?n.open=!0:n.open=!1}),window.addEventListener("DOMContentLoaded",e=>{let n=document.querySelector("#burger-content");window.innerWidth>=1024?n.open=!0:n.open=!1});
+window.addEventListener('resize', (e) => {
+    let n = document.querySelector('#burger-content')
+    window.innerWidth >= 1024 ? (n.open = !0) : (n.open = !1)
+}),
+    window.addEventListener('DOMContentLoaded', (e) => {
+        let n = document.querySelector('#burger-content')
+        window.innerWidth >= 1024 ? (n.open = !0) : (n.open = !1)
+    })
 //# sourceMappingURL=index.2de562a6.js.map
